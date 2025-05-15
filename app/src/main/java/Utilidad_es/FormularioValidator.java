ac5b674ba4ab2bae1174556ddad80b02d38cd1f7
fl1pc05 <fl1pc05@estudiantes.anai.lan>
2025-05-15 08:51:07 -0500

    Primer commit: subida de validaciones

---
+++ app/src/main/java/Utilidad_es/FormularioValidator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utilidad_es;
+
+/**
+ *
+ * @author fl1pc05
+ */
+public class FormularioValidator {
+    
+}
